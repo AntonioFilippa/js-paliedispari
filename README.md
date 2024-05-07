c83ff97159a359ff7f796b865fd8918a3bd0b527
antoniofilippa94@gmail.com <antoniofilippa94@gmail.com>
2024-05-07 14:46:30 +0200

    [END]

---
+++ README.md
@@ -0,0 +1,23 @@
+
+
+
+
+
+Chiedere all’utente di inserire una parola
+
+PROMPT UTENTE
+
+CONTROLLO SE LA PAROLA HA SPAZI E LI RIMUOVO 
+
+
+
+UTILIZZANDO IL METODO SPLIT,REVERSE,JOIN INVERTO LA PAROLA 
+
+
+
+
+
+
+
+
+Creare una funzione per capire se la parola inserita è palindroma
--- js/main.js
+++ js/main.js
@@ -1,7 +1,43 @@
-function èPalindroma(parola)
-{
-    //RIMUOVO SPAZI E CONVERTO TUTTO IN MINUSCOLO
+'use strict';
 
-    parola= parola.replace( / /g, '').toLoweCase();
+function isPalindroma(parola) {
     
+    // RIMUOVO GLI SPAZI E FACCIO MINUSCOLO
+    parola = parola.trim().toLowerCase();
+  
+    // GIRO LA STRINGA
+    const parolaInvertita = parola.split('').reverse().join('');
+
+    // COMPARO LE DUE PAROLE
+    return parola === parolaInvertita;
+  }
+
+  
+ // Ciclo per ripetere l'input in caso di errore
+while (true) {
+    // Richiesta di input all'utente
+    const parolaUtente = prompt('Inserisci una parola (o scrivi "esci" per terminare): ');
+  
+    // Controllo per terminare il programma
+    if (parolaUtente.toLowerCase() === 'esci') {
+      break;
+    }
+  
+// Controllo se è vuoto o un numero
+if (parolaUtente === '') {
+    alert('Errore: non hai inserito nessuna parola.');
+    console.log('Errore: l\'input è vuoto.');
+  } else if (!isNaN(parseInt(parolaUtente))) {
+    alert('Errore: non puoi inserire un numero.');
+    console.log('Errore: l\'input deve essere una parola.');
+  }else {
+    // Se non è vuoto e non è un numero, verifica se è palindroma
+    if (isPalindroma(parolaUtente)) {
+      alert('La parola è un palindromo!');
+      console.log(`${parolaUtente} è un palindromo!`);
+    } else {
+      alert('La parola non è un palindromo.');
+      console.log(`${parolaUtente} non è un palindromo.`);
+    }
+  }
 }
